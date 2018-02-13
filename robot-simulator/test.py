35ce8577ede0c9b928c74b3c95b33696a6bb6ff1
Alexander Niederbühl <a.niederbuehl@gmail.com>
2018-02-14 00:15:02 +0100

    robot-simulator: User test loader to preserve execution order of tests
    The order in which the test methods have been defined decides the
    execution order. Renaming the tests to define the order is not needed
    anymore.

--- robot-simulator/test.py
+++ robot-simulator/test.py
@@ -1,68 +1,68 @@
 import unittest
-
 from robot_simulator import Robot, NORTH, EAST, SOUTH, WEST
 
 
 class RobotTests(unittest.TestCase):
-    def test_1_init(self):
+
+    def test_init(self):
         robot = Robot()
         self.assertEqual(robot.coordinates, (0, 0))
         self.assertEqual(robot.bearing, NORTH)
 
-    def test_2_setup(self):
+    def test_setup(self):
         robot = Robot(SOUTH, -1, 1)
         self.assertEqual(robot.coordinates, (-1, 1))
         self.assertEqual(robot.bearing, SOUTH)
 
-    def test_3_turn_right(self):
+    def test_turn_right(self):
         robot = Robot()
         for direction in [EAST, SOUTH, WEST, NORTH]:
             robot.turn_right()
             self.assertEqual(robot.bearing, direction)
 
-    def test_4_turn_left(self):
+    def test_turn_left(self):
         robot = Robot()
         for direction in [WEST, SOUTH, EAST, NORTH]:
             robot.turn_left()
             self.assertEqual(robot.bearing, direction)
 
-    def test_5_advance_positive_north(self):
+    def test_advance_positive_north(self):
         robot = Robot(NORTH, 0, 0)
         robot.advance()
         self.assertEqual(robot.coordinates, (0, 1))
         self.assertEqual(robot.bearing, NORTH)
 
-    def test_6_advance_positive_east(self):
+    def test_advance_positive_east(self):
         robot = Robot(EAST, 0, 0)
         robot.advance()
         self.assertEqual(robot.coordinates, (1, 0))
         self.assertEqual(robot.bearing, EAST)
 
-    def test_7_advance_negative_south(self):
+    def test_advance_negative_south(self):
         robot = Robot(SOUTH, 0, 0)
         robot.advance()
         self.assertEqual(robot.coordinates, (0, -1))
         self.assertEqual(robot.bearing, SOUTH)
 
-    def test_8_advance_positive_west(self):
+    def test_advance_positive_west(self):
         robot = Robot(WEST, 0, 0)
         robot.advance()
         self.assertEqual(robot.coordinates, (-1, 0))
         self.assertEqual(robot.bearing, WEST)
 
-    def test_9_simulate_prog1(self):
+    def test_simulate_prog1(self):
         robot = Robot(NORTH, 0, 0)
         robot.simulate("LAAARALA")
         self.assertEqual(robot.coordinates, (-4, 1))
         self.assertEqual(robot.bearing, WEST)
 
-    def test_z10_simulate_prog2(self):
+    def test_simulate_prog2(self):
         robot = Robot(EAST, 2, -7)
         robot.simulate("RRAAAAALA")
         self.assertEqual(robot.coordinates, (-3, -8))
         self.assertEqual(robot.bearing, SOUTH)
 
-    def test_z11_simulate_prog3(self):
+    def test_simulate_prog3(self):
         robot = Robot(SOUTH, 8, 4)
         robot.simulate("LAAARRRALLLL")
         self.assertEqual(robot.coordinates, (11, 5))
@@ -70,4 +70,11 @@ class RobotTests(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    # Sort test methods by order of definition
+    # https://stackoverflow.com/a/18499093
+    loader = unittest.TestLoader()
+    get_line_number = lambda f: getattr(RobotTests, f).__code__.co_firstlineno
+    line_compare = lambda a, b: get_line_number(a) - get_line_number(b)
+    loader.sortTestMethodsUsing = line_compare
+
+    unittest.main(testLoader=loader)
